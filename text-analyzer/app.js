c144ee264d300a8dcc44de6fe477f49d47caa9dd
Kyle Rogers <kylerogers334@gmail.com>
2017-05-11 18:51:32 +0000

    reformat app.js

--- text-analyzer/app.js
+++ text-analyzer/app.js
@@ -1,41 +1,45 @@
+/* global $ */
+
 const countText = (arr) => {
-  let count = 0;
-  arr.forEach(index => {
+    let count = 0;
+    arr.forEach(index => {
     count++;
-  });
-  return count;
-}
+    });
+    return count;
+};
 
 const uniqueCount = (arr) => {
-  const count = {};
-
-  arr.forEach(s => {
-    if (count[s]){
-      count[s] += 1;
-    } else {
-      count[s] = 1;
-    }
-  })
+    const count = {};
+    
+    arr.forEach(s => {
+        if (count[s]){
+            count[s] += 1;
+        } else {
+            count[s] = 1;
+        }
+    });
 
     return Object.keys(count).length;
-}
+};
 
 const averageLength = (arr) => {
-  let count = 0;
+    let count = 0;
+    
+    arr.forEach(s => {
+        count+= s.length;
+    });
+    
+    // Format average length to 2 decimal places.
+    return Number(Math.round(count/arr.length + 'e+2') + "e-2");
+};
 
-  arr.forEach(s => {
-    count+= s.length;
-  })
-
-  return Number(Math.round(count/arr.length + 'e+2') + "e-2");
-}
 $(() => {
-  $('form').submit(()=> {
-    event.preventDefault();
-    const textArea = $('form textarea').val().split(' ');
-
-    $('#word-count').text(countText(textArea));
-    $('#unique-count').text(uniqueCount(textArea));
-    $('#average-length').text(averageLength(textArea));
-  })
-})
+    $('form').submit(()=> {
+        event.preventDefault();
+        const textArea = $('form textarea').val().split(' ');
+        
+        $('#word-count').text(countText(textArea));
+        $('#unique-count').text(uniqueCount(textArea));
+        $('#average-length').text(averageLength(textArea));
+    });
+});
